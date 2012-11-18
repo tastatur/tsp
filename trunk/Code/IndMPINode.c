436d3c1123139981c25bd35b4b270c206653f7c0
anuprem.c@gmail.com <anuprem.c@gmail.com@8a157cf9-c28c-f359-bfa0-80fa81479998>
2012-11-18 05:30:16 +0000

    MPI files

---
+++ trunk/Code/IndMPINode.c
@@ -0,0 +1,24 @@
+#include <omp.h>
+#include "IndMPINode.h"
+
+
+void ProcessRoute(int** localPopulation, int numberOfTours, int** coords)
+{
+  int tour;
+  int blockSize;
+  omp_set_num_threads(numberOfTours);
+  
+
+  #pragma omp parallel
+  {
+    #pragma omp default(none) shared(localPopulation, NUM_CITIES, numberOfTours) private(tour)
+    for(tour = 0; tour < numberOfTours; tour++)
+    {
+      int numLocalCities = BLOCKSIZE ;
+      int numLocalCitiesFinal = NUM_CITIES - (3 * BLOCKSIZE);
+  
+      TSPSwapRun(tour, coords);
+    }
+  }
+}
+
