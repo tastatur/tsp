92db70fb85ca4b8e6d443d72157d28bc5a7b53b9
anuprem.c <anuprem.c@gmail.com>
2012-11-26 21:03:39 +0000

    new code

--- Code/IndMPINode.c
+++ Code/IndMPINode.c
@@ -15,85 +15,70 @@ void ProcessRoute(int* localPopulation, int numberOfTours,const int* coords)
   omp_set_num_threads(numberOfTours);
   int fitness[numberOfTours];
   
-#pragma omp parallel default(shared) shared(localPopulation, numCities, numberOfTours , coords , fitness) private(tour,prevX , prevY , distance , i)
-{
-#pragma omp for
+  #pragma omp parallel default(shared) shared(localPopulation, numCities, numberOfTours , coords , fitness) private(tour,prevX , prevY , distance , i)
+  {
+    #pragma omp for
     for(tour = 0; tour < numberOfTours; tour++)
     {
-    //  int numLocalCities = BLOCKSIZE ;
-     // int numLocalCitiesFinal = NUM_CITIES - (3 * BLOCKSIZE);
-  	printf("\nID IS %d" , tour);
-    TSPSwapRun((int *)(localPopulation + numCities*tour) , (const int*)coords);
+      /* printf("\nID IS %d" , tour); */
+      TSPSwapRun((int *)(localPopulation + numCities*tour) , (const int*)coords);
+      prevX = (coords + (2 * (localPopulation + numCities*tour)[0]))[0]; 
+      prevY = (coords + (2 * (localPopulation + numCities*tour)[0]))[1];
 
-    prevX = (coords + (2 * (localPopulation + numCities*tour)[0]))[0]; 
-
-    //coords[((localPopulation + numCities*tour)[0])][0]; //(coords + (2 * (localPopulation + numCities*tour)[0]))[0];
-
-    prevY = (coords + (2 * (localPopulation + numCities*tour)[0]))[1];
-
-    //coords[((localPopulation + numCities*tour)[0])][1]; //(coords + (2 * (localPopulation + numCities*tour)[0]))[1];
-
-    for(i = 0; i < numCities; i++)
-    {
-      city = (localPopulation + numCities*tour)[i];
-      printf ("City %d " , city);
-      distance += (float)((coords + (city*2))[1] - prevY) * ((coords + (city*2))[1] - prevY)
-                   + (float)((coords + (city*2))[0] - prevX) * ((coords + (city*2))[0] - prevX);
-      prevX = (coords + (city*2))[0];
-      prevY = (coords + (city*2))[1];
-    }
-	printf("\nfitness %d" , distance);
+      for(i = 0; i < numCities; i++)
+      {
+        city = (localPopulation + numCities*tour)[i];
+        /* printf ("City %d " , city); */
+        distance += (float)((coords + (city*2))[1] - prevY) * ((coords + (city*2))[1] - prevY)
+                 +  (float)((coords + (city*2))[0] - prevX) * ((coords + (city*2))[0] - prevX);
+        prevX = (coords + (city*2))[0];
+        prevY = (coords + (city*2))[1];
+      }
+	/* printf("\nfitness %d" , distance); */
 	fitness[tour] = distance;
     }
-}
+  }
 
-//	firstMax = 0 ;
-//	secondMax = 0;
-
-    for ( i = 0 ; i < numberOfTours ; i ++ )
-	{
-	printf("came in loop");
-		if( fitness[i] < firstMax ) {
-			secondMax = firstMax;
-			firstMax = fitness[i];}
-
-		else if (fitness[i] < secondMax)
-			secondMax = fitness[i];
-	}
-	printf("firsMax %d " , firstMax);
-	printf("secondMax %d " , secondMax);
-	
-    populationStartIndex = 0;
-    for ( i = 0 ; i < numCities ; i++ )
-	{
-		(localPopulation + numCities*(populationStartIndex))[i] = (localPopulation + numCities*firstMax)[i];
-		(localPopulation + numCities*(populationStartIndex + 1))[i] = (localPopulation + numCities*secondMax)[i];
-	}
-
+  for ( i = 0 ; i < numberOfTours ; i ++ )
+  {
+    /* printf("came in loop"); */
+    if( fitness[i] < firstMax ) {
+    secondMax = firstMax;
+    firstMax = fitness[i];}
+    
+    else if (fitness[i] < secondMax)
+      secondMax = fitness[i];
+  }
+  /* printf("firsMax %d " , firstMax); */
+  /* printf("secondMax %d " , secondMax); */
+  
+  populationStartIndex = 0;
+  for ( i = 0 ; i < numCities ; i++ )
+  {
+    (localPopulation + numCities*(populationStartIndex))[i] = (localPopulation + numCities*firstMax)[i];
+    /* (localPopulation + numCities*(populationStartIndex + 1))[i] = (localPopulation + numCities*secondMax)[i]; */
+  }
+  
 }
 
 
-/*int main()
+int main()
 {
   int i;
   int tour[NUM_CITIES] = {0, 2, 1 ,3, 4, 6,5, 7, 8, 10, 9, 11, 12, 14, 13, 15 , 16};
-  int** coords = (int**)malloc(NUM_CITIES * sizeof(int *));
-  for(i = 0 ; i < NUM_CITIES; i++)
-  {
-    coords[i] = (int *)malloc(2 * sizeof(int));
-  }
+  int* coords = (int *)malloc(2 * NUM_CITIES * sizeof(int *));
 
   for(i = 0; i < NUM_CITIES; i++)
   {
-    coords[i][0] = i;
-    coords[i][1] = i;
+    coords[2 * i]= i;
+    coords[(2 * i) + 1] = i;
   }
-  ProcessRoute((int*)tour ,1 ,  (const int**)coords);
-  //TSPSwapRun((int *)tour, 5 , (const int**)coords);
-}*/
+  ProcessRoute((int *)tour ,1 , (const int *)coords);
+  for(i = 0; i < NUM_CITIES; i++)
+    printf("%d ", tour[i]);
+  printf("\n");
+}
 
 
-
-//	ProcessRoute((int*)0,5,(int*)0);
 
 
